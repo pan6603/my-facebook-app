8c03c7fe32de4e3d5b8b1f56415a67643a2a2121
pan6603 <pan5158a@gmail.com>
2025-12-09 16:23:11 +0900

    로그인 페이지 UI 구현

--- src/styles/login/layout/LeftPanel.style.js
+++ src/styles/login/layout/LeftPanel.style.js
@@ -2,8 +2,99 @@ import styled from "styled-components";
 
 
 export const LeftPanelDiv = styled.div`
+    position: relative;
     max-width: 788px;
     width: 100%;
     height: 690px;
-    background-color: orange;
+    display: flex;
 `
+
+export const BrandingSectionDiv = styled.div`
+    position: absolute;
+    max-width: 330px;
+    width: 100%;
+    height: 590px;
+    padding-bottom: 40px;
+    padding-top: 40px;
+
+`
+
+export const BrandingSection = styled.div`
+    max-width: 330px;
+    width: 100%;
+    height: 510px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between; 
+    align-items: center;
+`
+
+export const TitleLogo = styled.div`
+    max-width: 330px;
+    width: 100%;
+    height: 72px;
+    padding-inline-start: 20px;
+    padding-inline-end: 20px;
+`
+
+export const LogoDiv = styled.div`
+    max-width: 92px;
+    width: 100%;
+    height: 72px;
+    padding-inline-start: 20px;
+`
+
+export const LogoSvg = styled.svg`
+    max-width: 72px;
+    width: 100%;
+    height: 72px;
+`
+
+
+export const BrandingSectionTextDiv = styled.div`
+    max-width: 330px;
+    width: 100%;
+    height: 196px;
+    padding-inline-start: 20px;
+    padding-inline-end: 20px;
+    z-index: 20px;
+`
+
+export const BrandingSectionText = styled.div`
+    max-width: 290px;
+    width: 100%;
+    height: 176px;
+`
+
+export const BrandingSectionTextSpan = styled.div`
+    max-width: 270px;
+    width: 100%;
+    height: 198px;
+    padding-inline-start: 20px;
+    padding-inline-end: 20px;
+    font-size: 50px;
+    font-weight: 600;
+    color: #000;
+`
+
+export const TextSpan = styled.div`
+    color: #1877F2;
+    text-align: start;
+    font-weight: 600;
+    font-size: 50px;
+`
+
+export const BackgroundPhoto = styled.img`
+    border-start-start-radius: 40px;
+    border-start-end-radius: 40px;
+    margin-inline-start: 20%;
+    border: 0px;
+    object-fit: contain;
+    max-width: 630px;
+    width: 100%;
+    height: calc(100vh - 70px);
+    min-height: 590px;
+`
+
+
+
